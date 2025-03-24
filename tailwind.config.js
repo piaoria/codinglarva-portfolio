e313c43c0d8bc8b181d20174d63ce67397e4c12f
HyoJin <cron.phj@gmail.com>
2025-03-24 10:28:25 +0900

    🔧chore: Tailwind 설정 및 폰트 추가
    - tailwind.config.js에서 새로운 폰트 패밀리 및 폰트 두께 추가
    - .vscode/settings.json에 색상 테마 설정 추가
    - Pretendard 폰트 파일 추가
    - layout.tsx에서 Pretendard 폰트 및 Bungee 폰트 Test 적용
    - page.tsx에서 제목 폰트 변경 및 불필요한 클래스 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-pretendard-regular)"],
+        bungee: ["var(--font-bungee-hairline)"],
+      },
+      fontWeight: {
+        thin: "100",
+        extralight: "200",
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+        black: "900",
+      },
+    },
   },
   plugins: [],
 };
