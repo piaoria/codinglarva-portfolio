8fa252bb8d7e732c73f2d6b66b8849b3f547221e
HyoJin <cron.phj@gmail.com>
2025-03-25 16:45:53 +0900

    🔧chore: Tailwind 설정 및 폰트 파일 정리
    - Pretendard 폰트 설정 수정
    - 사용하지 않는 Pretendard 폰트 파일 삭제 및 PretendardVariable 폰트 파일 추가
    - 배경 색상 및 폰트 변수 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: ["class"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-pretendard-regular)"],
+        pretendard: ["var(--font-pretendard)"],
         bungee: ["var(--font-bungee-hairline)"],
-      },
-      fontWeight: {
-        thin: "100",
-        extralight: "200",
-        light: "300",
-        normal: "400",
-        medium: "500",
-        semibold: "600",
-        bold: "700",
-        extrabold: "800",
-        black: "900",
       },
     },
   },
   plugins: [],
 };
+
+export default config;
