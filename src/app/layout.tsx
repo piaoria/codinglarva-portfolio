814db0ceb3494500076d77939c5529d62b0a9d42
HyoJin <cron.phj@gmail.com>
2025-03-25 23:21:09 +0900

    🔧chore: 글로벌 CSS 및 레이아웃 수정
    - 불필요 Pretendard 폰트 설정 제거
    - 기본 및 다크모드 테마 색상 변수 수정
    - layout.tsx에서 transition 속성 수정 (disableTransitionOnChange 변경)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
+          disableTransitionOnChange={false}
         >
           <CustomCursor />
           <Header />
