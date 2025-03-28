db20f0a6e4fda2f7993401200f3980c86707017f
HyoJin <cron.phj@gmail.com>
2025-03-28 10:56:37 +0900

    🔧chore: About 섹션 텍스트 및 레이아웃 수정
    - About 섹션의 텍스트 구조를 개선하여 가독성 향상
    - 왼쪽 텍스트 영역의 flex 비율 조정 및 여백 추가

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -8,22 +8,26 @@ export default function About() {
     >
       <div className="w-full max-w-7xl flex flex-col md:flex-row items-center justify-between gap-8 md:gap-16 lg:gap-32">
         {/* 왼쪽 텍스트 */}
-        <div className="flex-1 min-w-0 flex justify-center md:justify-end">
-          <div className="text-4xl md:text-6xl leading-tight font-thin">
-            안녕하세요
-            <span className="text-[#F78535] font-black">!</span>
-            <br />
-            <span className="text-[#F78535] font-black">F</span>
-            ront
-            <span className="text-[#F78535] font-black">E</span>nd 개발자
-            <br />
-            <span className="font-black">박효진</span> 입니다
-            <span className="text-[#F78535] font-black">.</span>
+        <div className="flex-[1.5] min-w-0 flex justify-center md:justify-end">
+          <div className="text-4xl md:text-6xl leading-tight font-thin space-y-4 md:space-y-6">
+            <div>
+              안녕하세요
+              <span className="text-[#F78535] font-black">!</span>
+            </div>
+            <div>
+              <span className="text-[#F78535] font-black">F</span>
+              ront
+              <span className="text-[#F78535] font-black">E</span>nd 개발자
+            </div>
+            <div>
+              <span className="font-black">박효진</span> 입니다
+              <span className="text-[#F78535] font-black">.</span>
+            </div>
           </div>
         </div>
 
         {/* 오른쪽 */}
-        <div className="flex-1 relative">
+        <div className="flex-[2] relative">
           {/* 배경 텍스트 */}
           <div className="absolute inset-0 flex items-center justify-center mb-24 md:mb-48">
             <h2 className="text-[50px] md:text-[80px] lg:text-[100px] z-10 font-bungee text-[#F78535] leading-none">
