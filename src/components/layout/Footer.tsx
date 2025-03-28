689fa03e95d7429b244c1593b8efe13a2dca9bff
HyoJin <cron.phj@gmail.com>
2025-03-28 11:01:06 +0900

    🔧chore: Footer 텍스트 크기 조정
    - Footer 컴포넌트의 텍스트 크기를 sm:text-xl로 변경

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full pt-12 pb-16 flex justify-center">
-      <p className="text-xl font-bungee">
+      <p className="text-xs sm:text-xl font-bungee">
         © 2025. Park Hyo jin. All rights reserved.
       </p>
     </footer>
