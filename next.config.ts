05424cc6c23951ef6f7e3ee696cf2a2efe6edb71
HyoJin <cron.phj@gmail.com>
2025-04-08 12:11:23 +0900

    ➕feat: 사이트맵 XML에 스타일시트 추가 및 헤더 설정
    - sitemap.xml 파일에 XSL 스타일시트 링크 추가
    - next.config.ts에 sitemap.xml에 대한 Content-Type 헤더 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,19 @@ const nextConfig: NextConfig = {
     });
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
