19c2b010721fd1da79f55e3fde6d62cf721516d8
HyoJin <cron.phj@gmail.com>
2025-04-11 23:45:19 +0900

    🔧fix: 기본 테마를 dark에서 light로 변경
    - RootLayout 컴포넌트의 기본 테마를 'dark'에서 'light'로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -100,7 +100,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange={false}
         >
